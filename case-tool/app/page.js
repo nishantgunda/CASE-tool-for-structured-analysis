73b90c786978f9bfd89341b2ef2c849bb8c16bc4
agent-reagent <avanish.srivats@gmail.com>
2024-08-21 09:49:58 +0530

    we start here!

--- case-tool/app/page.js
+++ case-tool/app/page.js
@@ -1,12 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <div className="main-container">
-        <div className="sidebar">
-
-        </div>
-     
+  return ( 
+    <div>hi</div>
   );
 }
